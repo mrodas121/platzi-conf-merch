7a5ab34288bfb75f857e31dad2a35ae6808d9115
mrodas121 <manuel.rodas121@gmail.com>
2021-11-02 22:14:56 -0600

    usestate para manejo de productos en carrito

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React,{useContext} from "react";
 import { Link } from "react-router-dom";
-
+import AppContext from "../context/AppContext";
 import "../styles/components/Header.css";
 
 const Header =()=>{
+    const {state} = useContext(AppContext);
+    const {cart} = state;
     return (
         <div className="Header">
             <h1 className="Header-title">
@@ -15,6 +17,7 @@ const Header =()=>{
                 <Link to="/checkout">
                     <i className="fas fa-shopping-basket" title="Checkout" />
                 </Link>
+                {cart.length > 0 && <div className="Header-alert">{cart.length}</div>}
             </div>
         </div>
     );
